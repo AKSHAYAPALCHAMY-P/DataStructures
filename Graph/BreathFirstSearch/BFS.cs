fc3da098d9f400a6460dfcd1431c2703a076085a
Akshaya <akshaya@astravor.com>
2025-11-08 18:47:25 +0530

    ADDED: Bresth First Search 2D Problem and Test file

---
+++ Graph/BreathFirstSearch/BFS.cs
@@ -0,0 +1,50 @@
+﻿namespace BreathFirstSearch;
+
+public class BFS
+{
+    public int ShortestPath(int[][] nArray)
+    {
+        var nRow = nArray.Length;
+        var nColumn = nArray[0].Length;
+
+        if (nArray[0][0] == 1 || nArray[nRow - 1][nColumn - 1] == 1)
+            return -1;
+
+        var bVisited = new bool[nRow, nColumn];
+        var nQueue = new Queue<(int nRows, int nColumns, int nDistance)>();
+
+        nQueue.Enqueue((0, 0, 0));
+        bVisited[0, 0] = true;
+
+        var nDirections = new[]
+        {
+            new[] { 1, 0 }, //down
+            new[] { -1, 0 }, //up
+            new[] { 0, 1 }, // right
+            new[] { 0, -1 } // left
+        };
+
+        while (nQueue.Count > 0)
+        {
+            var (nRows, nColumns, nDistance) = nQueue.Dequeue();
+            if (nRows == nRow - 1 && nColumns == nColumns - 1) return nDistance;
+
+            foreach (var dis in nDirections)
+            {
+                var nRowsForTraverse = nRows + dis[0];
+                var nColumnsTraverse = nColumns + dis[1];
+
+                if (nRowsForTraverse > 0 && nColumnsTraverse <nRow &&
+                    nColumnsTraverse >= 0 && nColumnsTraverse < nColumn &&
+                    nArray[nRowsForTraverse][nColumnsTraverse] == 0 &&
+                    !bVisited[nRowsForTraverse, nColumnsTraverse])
+                {
+                    nQueue.Enqueue((nRowsForTraverse, nColumnsTraverse, nDistance + 1));
+                    bVisited[nRowsForTraverse, nColumnsTraverse] = true;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ Graph/BreathFirstSearch/ReadMe.md
@@ -0,0 +1,34 @@
+﻿## BFS
+### 2D Array:
+
+Problem: “Shortest Path in a Binary Matrix” (Easy version)
+Problem statement:
+
+You are given a 2D grid of size n x m filled with:
+
+0 → free cell (you can move here)
+
+1 → blocked cell (you cannot move here)
+
+You start at the top-left corner (0, 0) and want to reach the bottom-right corner (n-1, m-1).
+
+You can move up, down, left, right (4 directions).
+
+Find the minimum number of steps required to reach the destination.
+If it’s not possible, return -1.
+
+## Example:
+Input:
+grid = [
+  [0, 1, 0],
+  [0, 0, 0],
+  [1, 0, 0]
+]
+
+Output: 4
+
+Explanation:
+
+Possible path:
+
+(0,0) → (1,0) → (1,1) → (1,2) → (2,2)
