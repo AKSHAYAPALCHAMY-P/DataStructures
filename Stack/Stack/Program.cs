4e2b79b4b5fd3a010f573447e4e0ef1fdcc564dd
Akshaya <132545615+AKSHAYAPALCHAMY-P@users.noreply.github.com>
2025-11-12 21:51:48 +0530

    ADDED: Stack basics (#2)
    Co-authored-by: Akshaya <akshaya@astravor.com>

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace stack;
+
+public class StackOperations
+{
+    public static void Main(string[] args)
+    {
+        var MyStack = new Stack<int>(5);
+
+        MyStack.Push(1);
+        MyStack.Push(2);
+        MyStack.Push(3);
+        MyStack.Push(4);
+        MyStack.Push(5);
+
+        foreach (var stack in MyStack) Console.WriteLine(stack + " ");
+
+        Console.WriteLine(MyStack.Pop());
+        Console.WriteLine(MyStack.Peek());
+
+        Console.WriteLine(MyStack.Contains(5));
+    }
+}
