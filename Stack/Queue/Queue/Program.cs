58843a09bd19de2319b080acc5cbee73cdc18979
Akshaya <akshaya@astravor.com>
2025-11-12 22:09:29 +0530

    ADDED: Queue Operations

---
+++ Stack/Queue/Queue/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Queue;
+
+public class Queue_Operations
+{
+    public static void Main(string[] args)
+    {
+        var Queue = new Queue<int>();
+
+        Queue.Enqueue(1);
+        Queue.Enqueue(2);
+        Queue.Enqueue(3);
+        foreach (var n in Queue) Console.WriteLine($"Initial Queue:{n}");
+
+        var nDeQueue = Queue.Dequeue();
+        Console.WriteLine(nDeQueue);
+    }
+}
